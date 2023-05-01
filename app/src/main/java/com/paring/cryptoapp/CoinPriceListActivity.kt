cb3887381046237de7321273bb37ae166120ce00
Roman <roman.mihail.p@gmail.com>
2023-05-01 15:23:47 +0300

    Final Commit

---
+++ app/src/main/java/com/paring/cryptoapp/CoinPriceListActivity.kt
@@ -0,0 +1,36 @@
+package com.paring.cryptoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.paring.cryptoapp.adapters.CoinInfoAdapter
+import com.paring.cryptoapp.databinding.ActivityCoinPriceListBinding
+import com.paring.cryptoapp.pojo.CoinPriceInfo
+
+class CoinPriceListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCoinPriceListBinding
+    private lateinit var viewModel: CoinViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCoinPriceListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val adapter = CoinInfoAdapter(this)
+        adapter.onCoinClickListener = object : CoinInfoAdapter.OnCoinClickListener {
+            override fun onCoinClick(coinPriceInfo: CoinPriceInfo) {
+                val intent = CoinDetailActivity.newIntent(
+                    this@CoinPriceListActivity,
+                    coinPriceInfo.fromSymbol
+                )
+                startActivity(intent)
+            }
+        }
+        binding.rvCoinPriceList.adapter = adapter
+        viewModel = ViewModelProvider(this)[CoinViewModel::class.java]
+        viewModel.priceList.observe(this, Observer {
+            adapter.coinInfoList = it
+        })
+    }
+}
